5c5ca1435f1844059b5aad0ef71f7cbc407e2a65
KagisoKat <110462927+KagisoKat@users.noreply.github.com>
2023-04-24 16:24:38 +0200

    styling signIn and register button

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,28 +1,50 @@
 <template>
-<h1>Create an Account</h1>
-<p><input  class=" text-black mt-3" type="email" placeholder="Email" v-model="email"/></p>
-<p><input class=" text-black mt-3" type="password" placeholder="Password" v-model="password"/></p>
-<p><button class="bg-red-700 rounded text-black mt-3 ml-2 p-2" @click="register">Submit</button></p>
+    <div class="ml-3">
+  <h1>Create an Account</h1>
+  <p>
+    <input
+      class="text-black mt-3"
+      type="email"
+      placeholder="Email"
+      v-model="email"
+    />
+  </p>
+  <p>
+    <input
+      class="text-black mt-3"
+      type="password"
+      placeholder="Password"
+      v-model="password"
+    />
+  </p>
+  <p>
+    <button
+      class="bg-red-700 rounded text-black mt-3 ml-2 p-2"
+      @click="register"
+    >
+      Submit
+    </button>
+  </p>
+</div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import { getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from "vue-router";
 const email = ref("");
 const password = ref("");
-const router = useRouter()
+const router = useRouter();
 
 const register = () => {
-    createUserWithEmailAndPassword(getAuth(), email.value, password.value)
+  createUserWithEmailAndPassword(getAuth(), email.value, password.value)
     .then((data) => {
-        console.log("successfully registered")
-        router.push('/sign-in')
+      console.log("successfully registered");
+      router.push("/sign-in");
     })
     .catch((error) => {
-        console.log(error.code);
-        alert(error.message);
+      console.log(error.code);
+      alert(error.message);
     });
 };
-
 </script>
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,42 +1,63 @@
 <template>
+    <div class="ml-3">
     <h1>Sign into Account</h1>
-    <p><input  class=" text-black mt-3" type="email" placeholder="Email" v-model="email"/></p>
-    <p><input class=" text-black mt-3" type="password" placeholder="Password" v-model="password"/></p>
+    <p>
+      <input
+        class="text-black mt-3"
+        type="email"
+        placeholder="Email"
+        v-model="email"
+      />
+    </p>
+    <p>
+      <input
+        class="text-black mt-3"
+        type="password"
+        placeholder="Password"
+        v-model="password"
+      />
+    </p>
     <p v-if="errMsg">{{ errMsg }}</p>
-    <p><button class="bg-red-700 rounded text-black mt-3 ml-2 p-2" @click="register">Submit</button></p>
-    </template>
+    <p>
+      <button
+        class="bg-red-700 rounded text-black mt-3 ml-2 p-2"
+        @click="register"
+      >
+        Submit
+      </button>
+    </p>
+</div>
+</template>
     
     <script setup>
-    import { ref } from "vue";
-    import { getAuth, signInWithEmailAndPassword} from "firebase/auth";
-    import { useRouter } from "vue-router";
-    const email = ref("");
-    const password = ref("");
-    const errMsg = ref() //error message
-    const router = useRouter() //get reference to our vue router
-    
-    const register = () => {
-        signInWithEmailAndPassword(getAuth(), email.value, password.value)
-        .then((data) => {
-            console.log("successfully signed in")
-            router.push('/')
-        })
-        .catch((error) => {
-            console.log(error.code);
-            switch (error.code) {
-                case "auth/invalid-email":
-                    errMsg.value = "Invalid eamil";
-                    break;
-                case "auth/user-not-found":
-                    errMsg.value = "No account with email was found";
-                case "auth/wrong-password":
-                    errMsg.value = " Password incorrect";
-                    break;
-                default:
-                    errMsg.value = "Email or password incorrect";
-                    
-            }
-        });
-    };
-  
-    </script>
+import { ref } from "vue";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "vue-router";
+const email = ref("");
+const password = ref("");
+const errMsg = ref(); //error message
+const router = useRouter(); //get reference to our vue router
+
+const register = () => {
+  signInWithEmailAndPassword(getAuth(), email.value, password.value)
+    .then((data) => {
+      console.log("successfully signed in");
+      router.push("/");
+    })
+    .catch((error) => {
+      console.log(error.code);
+      switch (error.code) {
+        case "auth/invalid-email":
+          errMsg.value = "Invalid eamil";
+          break;
+        case "auth/user-not-found":
+          errMsg.value = "No account with email was found";
+        case "auth/wrong-password":
+          errMsg.value = " Password incorrect";
+          break;
+        default:
+          errMsg.value = "Email or password incorrect";
+      }
+    });
+};
+</script>
